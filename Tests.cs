2e3f2132287184a58b48eee640b83b36ff7ee062
iSereban <web-podderjka@mail.ru>
2024-06-23 16:21:22 +0300

    Наполняю масив строк значением - Вызов функции для формирования нового массива - Вывод исходного массива и результата

--- Tests.cs
+++ Tests.cs
@@ -1,4 +1,24 @@
-static string[] FilterStringsByLength(string[] array, int maxLength)
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите количество слов");
+        int count = Convert.ToInt32(Console.ReadLine());
+
+        string[] arrayString = new string[count];
+        for (int i = 0; i < count; i++)
+        {
+            Console.WriteLine("Введите слово");
+            arrayString[i] = Console.ReadLine();
+
+        }
+
+        string[] resultArray = FilterStringsByLength(arrayString, 3);
+
+        Console.WriteLine($"[{string.Join(", ", arrayString)}] -> [{string.Join(", ", resultArray)}]");
+    }
+
+    static string[] FilterStringsByLength(string[] array, int maxLength)
 {
     int count = 0;
     foreach (string str in array)
