dd196db9b4e86ce2ccec83ecdb4aa3e1fcd09431
iSereban <web-podderjka@mail.ru>
2024-06-23 16:09:14 +0300

    Подсчет количества строк, удовлетворяющих условию и создание нового массива

--- Tests.cs
+++ Tests.cs
@@ -1,4 +1,14 @@
 static string[] FilterStringsByLength(string[] array, int maxLength)
 {
-    
+    int count = 0;
+    foreach (string str in array)
+    {
+        if (str.Length <= maxLength)
+        {
+            count++;
+        }
+    }
+
+    string[] result = new string[count];
+    int index = 0;
 }
